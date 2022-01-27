bba0ca0518927d4ecb82e4d9c63cb27f28304f57
Matthew Knox <matthew.j.knox@duke.edu>
2022-01-27 14:51:19 -0500

    added dependencies for CellState, shape

--- src/main/java/cellsociety/Main.java
+++ src/main/java/cellsociety/Main.java
@@ -1,11 +1,12 @@
 package cellsociety;
 
+import cellsociety.CellState.*;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.scene.shape.Circle;
+import javafx.scene.shape.*;
 
 
 /**
