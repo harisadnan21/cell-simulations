8f7cbef791ef1dfef0353d1c69af8f4b6e322bca
Haris Adnan <haris.adnan@duke.edu>
2022-01-27 16:40:25 -0500

    made changes to Simulation.java:

---
+++ src/main/java/cellsociety/Simulation.java
@@ -0,0 +1,37 @@
+package cellsociety;
+
+public class Simulation{
+  public Simulation(){
+
+  }
+  private void setUpSimulation(){
+
+  }
+  private void step(){
+
+  }
+  private void getParameter(){
+
+  }
+  private void setParameter(){
+
+  }
+  private void start(){
+
+  }
+  private void stop(){
+
+  }
+  private void save(){
+
+  }
+  private void load(){
+
+  }
+  private void speedUp(){
+
+  }
+  private void slowDown(){
+    
+  }
+}
