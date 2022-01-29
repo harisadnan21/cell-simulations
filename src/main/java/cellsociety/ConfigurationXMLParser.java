8b251f84fd0d9e6d01accbb84c75d201564ab88d
Edison Ooi <edison.ooi@duke.edu>
2022-01-28 22:14:59 -0500

    Borrowed XML parsing logic from Duvall's spike_cellsociety project

--- src/main/java/cellsociety/ConfigurationXMLParser.java
+++ src/main/java/cellsociety/ConfigurationXMLParser.java
@@ -1,120 +1,101 @@
 package cellsociety;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-
+/**
+ * This class handles parsing XML files and returning a completed object.
+ *
+ * @author Rhondu Smithwick
+ * @author Robert C. Duvall
+ */
 public class ConfigurationXMLParser {
-    private int numRows;
-    private int numColumns;
-
-    private int simulationType;
-
-    private String title;
-    private String author;
-    private String description;
-
-    private int[][] cellConfig;
-
-    public void parseXML(String filename) {
-        String filePath = "./data/" + filename + ".xml";
-
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-        try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(filePath);
-            Element rootElement = doc.getDocumentElement();
-            NodeList nodes = rootElement.getChildNodes();
-
-            for (int i = 0; i < nodes.getLength(); i++) {
-                if (nodes.item(i).getNodeType() == Node.ELEMENT_NODE) {
-                    Element currentElement = (Element) nodes.item(i);
-
-                    if(currentElement.getNodeName().equals("SimulationParameters")) {
-                        //handle simulation parameters
-                    } else {
-                        switch (currentElement.getNodeName()) {
-                            case "NumberOfRows":
-                                numRows = Integer.parseInt(currentElement.getTextContent());
-                                break;
-                            case "NumberOfColumns":
-                                numColumns = Integer.parseInt(currentElement.getTextContent());
-                                break;
-                            case "SimulationType":
-                                simulationType = Integer.parseInt(currentElement.getTextContent());
-                                break;
-                            case "Title":
-                                title = currentElement.getTextContent();
-                                break;
-                            case "Author":
-                                author = currentElement.getTextContent();
-                                break;
-                            case "Description":
-                                description = currentElement.getTextContent();
-                                break;
-                            case "StartingConfig":
-                                setCellConfig(currentElement.getTextContent());
-                                break;
-
-                        }
-                    }
+    // Readable error message that can be displayed by the GUI
+    public static final String ERROR_MESSAGE = "XML file does not represent %s";
+    // name of root attribute that notes the type of file expecting to parse
+    private final String TYPE_ATTRIBUTE;
+    // keep only one documentBuilder because it is expensive to make and can reset it before parsing
+    private final DocumentBuilder DOCUMENT_BUILDER;
 
 
-//                    if (el.getNodeName().contains("staff")) {
-//                        String name = el.getElementsByTagName("name").item(0).getTextContent();
-//                        String phone = el.getElementsByTagName("phone").item(0).getTextContent();
-//                        String email = el.getElementsByTagName("email").item(0).getTextContent();
-//                        String area = el.getElementsByTagName("area").item(0).getTextContent();
-//                        String city = el.getElementsByTagName("city").item(0).getTextContent();
-
-                }
-            }
-
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-
-//        System.out.println(numRows);
-//        System.out.println(numColumns);
-//        System.out.println(simulationType);
-//        System.out.println(title);
-//        System.out.println(author);
-//        System.out.println(description);
-        for (int i = 0; i < cellConfig.length; i++) {
-            for (int j = 0; j < cellConfig[0].length; j++) {
-                System.out.print(cellConfig[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-
-
-
-
+    /**
+     * Create parser for XML files of given type.
+     */
+    public ConfigurationXMLParser (String type) throws XMLException {
+        DOCUMENT_BUILDER = getDocumentBuilder();
+        TYPE_ATTRIBUTE = type;
     }
 
-    private void setCellConfig(String config) {
-        cellConfig = new int[numRows][numColumns];
-
-        String[] configChars = config.split(" ");
-
-        if(cellConfig.length * cellConfig[0].length != configChars.length) {
-            System.out.println("Incorrect config string length");
-            return;
+    /**
+     * Get data contained in this XML file as an object
+     */
+    public SimulationData getSimulationData (File dataFile) throws XMLException {
+        Element root = getRootElement(dataFile);
+        if (!isValidFile(root, SimulationData.DATA_TYPE)) {
+            throw new XMLException(ERROR_MESSAGE, SimulationData.DATA_TYPE);
+        }
+        // read data associated with the fields given by the object
+        Map<String, String> genericSimulationValues = new HashMap<>();
+        for (String field : SimulationData.GENERIC_DATA_FIELDS) {
+            genericSimulationValues.put(field, getTextValue(root, field));
         }
 
-        for (int i = 0; i < cellConfig.length; i++) {
-            for (int j = 0; j < cellConfig[0].length; j++) {
-                cellConfig[i][j] = Integer.parseInt(configChars[i + j]);
-            }
+        Map<String, String> specificSimulationParams = new HashMap<>();
+
+
+        return new SimulationData(genericSimulationValues, specificSimulationParams);
+    }
+
+    // get root element of an XML file
+    private Element getRootElement (File xmlFile) throws XMLException {
+        try {
+            DOCUMENT_BUILDER.reset();
+            Document xmlDocument = DOCUMENT_BUILDER.parse(xmlFile);
+            return xmlDocument.getDocumentElement();
+        }
+        catch (SAXException | IOException e) {
+            throw new XMLException(e);
+        }
+    }
+
+    // returns if this is a valid XML file for the specified object type
+    private boolean isValidFile (Element root, String type) {
+        return getAttribute(root, TYPE_ATTRIBUTE).equals(type);
+    }
+
+    // get value of Element's attribute
+    private String getAttribute (Element e, String attributeName) {
+        return e.getAttribute(attributeName);
+    }
+
+    // get value of Element's text
+    private String getTextValue (Element e, String tagName) {
+        NodeList nodeList = e.getElementsByTagName(tagName);
+        if (nodeList.getLength() > 0) {
+            return nodeList.item(0).getTextContent();
+        }
+        else {
+            return "";
+        }
+    }
+
+    // boilerplate code needed to make ANY DocumentBuilder
+    private DocumentBuilder getDocumentBuilder () throws XMLException {
+        try {
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        }
+        catch (ParserConfigurationException e) {
+            throw new XMLException(e);
         }
     }
 }
