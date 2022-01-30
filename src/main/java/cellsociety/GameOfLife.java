86d959931b7842cd5afd2aefc6a2847a9fba668c
Matthew Knox <matthew.j.knox@duke.edu>
2022-01-30 12:14:32 -0500

    finished Game Of Life algorithm

---
+++ src/main/java/cellsociety/GameOfLife.java
@@ -0,0 +1,53 @@
+package cellsociety;
+
+
+import cellsociety.CellState.GameOfLifeState;
+import java.util.Collection;
+
+public class GameOfLife extends CellularAutomataAlgorithm {
+
+
+
+  /**
+   * Class constructor.
+   *
+   * @param data SimulationData record that contains the data obtained from parsing the configuration
+   *             XML
+   */
+  public GameOfLife(SimulationData data) {
+    super(data);
+  }
+
+  @Override
+  protected void setUpSimulationParameters() {
+
+  }
+
+  @Override
+  public CellState runAlgorithm(Grid g, Cell c) {
+    GameOfLifeState currentState = (GameOfLifeState) c.getState();
+    Collection<Cell> neighbors = c.getNeighbors();
+
+    //get the number of live neighbors
+    int liveCount = 0;
+    for(Cell neighbor: neighbors) {
+      GameOfLifeState neighborState = (GameOfLifeState) neighbor.getState();
+      switch(neighborState) {
+        case Live -> liveCount++;
+      }
+    }
+
+    switch(currentState) {
+      case Dead -> {
+        if(liveCount == 3) return GameOfLifeState.Live;
+        return GameOfLifeState.Dead;
+      }
+      case Live -> {
+        if(liveCount == 2 || liveCount == 3) return GameOfLifeState.Live;
+        return GameOfLifeState.Dead;
+      }
+    }
+    return null;
+  }
+
+}
