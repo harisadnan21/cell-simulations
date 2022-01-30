14a3176e2c58a8246f53da8557ce9fd7e977c31b
Edison Ooi <edison.ooi@duke.edu>
2022-01-30 17:02:40 -0500

    Created GridView class and constructor

---
+++ src/main/java/cellsociety/GridView.java
@@ -0,0 +1,19 @@
+package cellsociety;
+
+import javafx.scene.layout.TilePane;
+
+public class GridView extends TilePane {
+
+  public GridView(int width, int height, int numRows, int numColumns) {
+    super();
+    setWidth(width);
+    setHeight(height);
+    setPrefRows(numRows);
+    setPrefColumns(numColumns);
+  }
+
+
+
+
+
+}
