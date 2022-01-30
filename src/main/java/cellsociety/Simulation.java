a53a92e6084603aa97f3186b1740e77887060206
Haris Adnan <ha109@duke.edu>
2022-01-29 22:26:21 -0800

    made initial changes

--- src/main/java/cellsociety/Simulation.java
+++ src/main/java/cellsociety/Simulation.java
@@ -1,11 +1,34 @@
 package cellsociety;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
+
 public class Simulation{
+  private Scene scene;
+  private Group root;
+
+
+  private double GRIDSTARTINGX;
+  private double GRIDSTARTINGY;
+  private double GRIDHEIGHT;
+  private double GRIDWIDTH;
+  private int GRIDCOLS;
+  private int GIDROWS;
+  private double GRIDSTROKEWIDTH;
   public Simulation(){
-
   }
-  private void setUpSimulation(){
 
+  private Scene setUpSimulation(int width, int height, Paint background, SimulationType simtype){
+    root = new Group();
+    CellState[][] initialStates = getInitialConfig();
+    Grid newgrid = new Grid(GRIDSTARTINGX, GRIDSTARTINGY,GIDROWS, GRIDCOLS, GRIDWIDTH, GRIDHEIGHT,  initialStates, GRIDSTROKEWIDTH);
+    root.getChildren().add(newgrid);
+    scene = new Scene(root, width, height, background);
+    return scene;
+  }
+  private CellState[][] getInitialConfig(){
+    return 0;
   }
   private void step(){
 
