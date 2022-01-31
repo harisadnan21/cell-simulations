bcd63f51f8a8d654cf18f22de27714704c1ea42a
Edison Ooi <edison.ooi@duke.edu>
2022-01-30 22:13:07 -0500

    Make CellView extend Rectangle

--- src/main/java/cellsociety/CellView.java
+++ src/main/java/cellsociety/CellView.java
@@ -2,22 +2,19 @@ package cellsociety;
 
 import javafx.scene.shape.Rectangle;
 
-public class CellView {
-    private final Rectangle rect;
+public class CellView extends Rectangle {
     private CellState currentState;
-    private CellState nextState;
 
-    private CellView(double x, double y, double width, double height, CellState initialState, double strokeWidth, CellState secondState) {
-        rect = new Rectangle();
-        rect.setX(x);
-        rect.setY(y);
-        rect.setWidth(width);
-        rect.setHeight(height);
-        rect.setFill(GridView.getFillColor(initialState));
-        rect.setStroke(GridView.getStrokeColor(initialState));
-        rect.setStrokeWidth(strokeWidth);
+    private CellView(double x, double y, double width, double height, CellState initialState, double strokeWidth) {
+
+        setX(x);
+        setY(y);
+        setWidth(width);
+        setHeight(height);
+        setFill(GridView.getFillColor(initialState));
+        setStroke(GridView.getStrokeColor(initialState));
+        setStrokeWidth(strokeWidth);
         currentState = initialState;
-        nextState = secondState;
     }
 
     protected void updateColor(CellView rectangle, CellState nextState){
