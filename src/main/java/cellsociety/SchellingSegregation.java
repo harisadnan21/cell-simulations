dc17131009dfbdb4c47587b6740386f4a6ba9a75
Matthew Knox <matthew.j.knox@duke.edu>
2022-02-01 23:44:03 -0500

    segregation model complete, tested

--- src/main/java/cellsociety/SchellingSegregation.java
+++ src/main/java/cellsociety/SchellingSegregation.java
@@ -44,17 +44,54 @@ public class SchellingSegregation extends CellularAutomataAlgorithm {
     }
   }
 
+  /*
+  Algorithm:
+  iterate through all agents, if they are not satisfied, find a new location
+    if a new location has been found, set cell.next to empty, set newlocation.next to agent
+    if no new location has been found, set cell.next to cell.current
+  Iterate through all empty cells, if cell.next is null, set it to empty
+   */
   @Override
-  public CellState runAlgorithm(Grid g, Cell c) {
+  public void runAlgorithm(Grid g) {
+    for(Cell[] cellArray: g.getCells()) {
+      for(Cell c: cellArray) {
+        SchellingSegregationState currentState = (SchellingSegregationState) c.getState();
+        if(currentState != SchellingSegregationState.Empty) {
+          if(!isSatisfied(c)) {
+            Cell nextLocation = findEmptyCell(g);
+            if(nextLocation == null) { //no new location has been found
+              c.assignNextState(currentState);
+            } else { //new location found
+              c.assignNextState(SchellingSegregationState.Empty);
+              nextLocation.assignNextState(currentState);
+            }
+          } else { //location is satisfied
+            c.assignNextState(c.getState());
+          }
+        }
+      }
+    }
+    for(Cell[] cellArray: g.getCells()) {
+      for(Cell c: cellArray) {
+        SchellingSegregationState currentState = (SchellingSegregationState) c.getState();
+        if(currentState == SchellingSegregationState.Empty) {
+          if(c.getNextState() == null) {
+            c.assignNextState(SchellingSegregationState.Empty);
+          }
+        }
+      }
+    }
+    /*
     switch((SchellingSegregationState)c.getState()) {
       case AgentA -> { return handleAgentAState(g,c); }
       case AgentB -> { return handleAgentBState(g,c); }
       case Empty -> { return handleEmptyState(g,c); }
     }
+     */
 
-    return null;
   }
 
+  /*
   private CellState handleEmptyState(Grid g, Cell c) {
     return SchellingSegregationState.Empty;
   }
@@ -80,6 +117,7 @@ public class SchellingSegregation extends CellularAutomataAlgorithm {
       return SchellingSegregationState.Empty;
     }
   }
+   */
 
   private boolean isSatisfied(Cell c) {
     int numInGroup = 0;
@@ -94,14 +132,18 @@ public class SchellingSegregation extends CellularAutomataAlgorithm {
       }
     }
     double proportionInGroup = ((double)numInGroup) / ((double)(numInGroup + numOutGroup));
+    if(Double.isNaN(proportionInGroup)) return true;
     return proportionInGroup >= satisfaction;
   }
 
+  //We want to find a cell that is both empty and has not been assigned yet
+  //If a cell is empty, it's state is empty
+  //If it has not been assigned yet, it's next state is null
   private Cell findEmptyCell(Grid g) {
     Cell[][] allCells = g.getCells();
     for(Cell[] cellArray: allCells) {
       for(Cell c: cellArray) {
-        if(c.getState() == WaTorState.Empty && (c.getNextState() == null || c.getNextState() == WaTorState.Empty)) //found empty cell
+        if(c.getState() == SchellingSegregationState.Empty && c.getNextState() == null) //found empty cell
           return c;
       }
     }
