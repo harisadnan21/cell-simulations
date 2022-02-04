d499eee18fd76c5b3d1fe5a83ff724d149fa7d1f
Edison Ooi <edison.ooi@duke.edu>
2022-02-03 21:59:30 -0500

    Added fields to retrieve neighborhood data

--- src/main/java/cellsociety/Model/SimulationData.java
+++ src/main/java/cellsociety/Model/SimulationData.java
@@ -5,14 +5,17 @@ import java.util.Map;
 
 public record SimulationData(int simulationType, int numRows, int numColumns, String title,
                              String description,
-                             String author, String startingConfig, Map<String, String> params) {
+                             String author, int numNeighborRows, int numNeighborColumns,
+                             String neighborConfig, String startingConfig,
+                             Map<String, String> params) {
 
   // Name in data file that will indicate it represents data for this type of object
   public static final String DATA_TYPE = "CellAutomata";
   // Field names expected to appear in data file holding values for this object. These fields are commons across all SimulationTypes
   public static final List<String> GENERIC_DATA_FIELDS = List.of("SimulationType", "NumberOfRows",
       "NumberOfColumns",
-      "Title", "Description", "Author", "StartingConfig");
+      "Title", "Description", "Author", "NumberOfNeighborRows", "NumberOfNeighborColumns",
+      "NeighborConfig", "StartingConfig");
 
   public SimulationData(Map<String, String> genericValues, Map<String, String> simulationParams) {
     this(Integer.parseInt(genericValues.get(GENERIC_DATA_FIELDS.get(0))),
@@ -21,9 +24,11 @@ public record SimulationData(int simulationType, int numRows, int numColumns, St
         genericValues.get(GENERIC_DATA_FIELDS.get(3)),
         genericValues.get(GENERIC_DATA_FIELDS.get(4)),
         genericValues.get(GENERIC_DATA_FIELDS.get(5)),
-        genericValues.get(GENERIC_DATA_FIELDS.get(6)),
+        Integer.parseInt(genericValues.get(GENERIC_DATA_FIELDS.get(6))),
+        Integer.parseInt(genericValues.get(GENERIC_DATA_FIELDS.get(7))),
+        genericValues.get(GENERIC_DATA_FIELDS.get(8)),
+        genericValues.get(GENERIC_DATA_FIELDS.get(9)),
         simulationParams);
   }
-
 
 }
