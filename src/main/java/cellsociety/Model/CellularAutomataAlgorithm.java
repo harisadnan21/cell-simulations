bee9026f0ea38452c720f4138b2d921ce86abbbf
Edison Ooi <edison.ooi@duke.edu>
2022-02-03 22:09:46 -0500

    Implemented functionality to calculate and store neighborhood config array

--- src/main/java/cellsociety/Model/CellularAutomataAlgorithm.java
+++ src/main/java/cellsociety/Model/CellularAutomataAlgorithm.java
@@ -29,12 +29,17 @@ public abstract class CellularAutomataAlgorithm {
   // Information for setting up grid of cells
   private int numRows;
   private int numColumns;
-  private CellState[][] initialCellConfig; //TODO: Change this to array of CellState
+  private CellState[][] initialCellConfig;
 
   // Attributions
   private String title;
   private String description;
   private String author;
+
+  // Information about neighborhood configuration
+  private int numNeighborRows;
+  private int numNeighborColumns;
+  private int[][] neighborhoodConfig;
 
   // Parameters specific to this simulation type, for example probCatch in Spreading of Fire simulations
   private Map<String, String> simulationParams;
@@ -49,12 +54,16 @@ public abstract class CellularAutomataAlgorithm {
     simulationType = data.simulationType();
     numRows = data.numRows();
     numColumns = data.numColumns();
+    numNeighborRows = data.numNeighborRows();
+    numNeighborColumns = data.numNeighborColumns();
     title = data.title();
     description = data.description();
     author = data.author();
     simulationParams = data.params();
 
     setUpSimulationParameters();
+
+    initializeNeighborConfig(data.neighborConfig());
 
     initializeCellConfig(data.startingConfig());
   }
@@ -67,7 +76,7 @@ public abstract class CellularAutomataAlgorithm {
   /**
    * Check if simulationParams contains all the needed parameters for this algorithm. If not, throw
    * an Exception.
-   *
+   * <p>
    * This should always be called on the first line of setUpSimulationParameters.
    */
   protected void checkSimulationParameters(List<String> SPECIFIC_PARAMS) {
@@ -85,9 +94,15 @@ public abstract class CellularAutomataAlgorithm {
 
   // Initializes an array representing the starting state of every cell in the simulation
   private void initializeCellConfig(String config) {
-    String trimmedConfig = config.trim().replaceAll("\\s+", " ");
+    String[] initialStates = getArrayFromConfigString(config);
+
+    if (numRows * numColumns != initialStates.length) {
+      throw new IllegalArgumentException(
+          "Cell config size does not match number of rows and columns!");
+    }
+
     initialCellConfig = new CellState[numRows][numColumns];
-    String[] initialStates = trimmedConfig.split(" ");
+
     CellState[] possibleStates = new CellState[0];
 
     switch (simulationType) {
@@ -100,9 +115,49 @@ public abstract class CellularAutomataAlgorithm {
 
     for (int i = 0; i < initialCellConfig.length; i++) {
       for (int j = 0; j < initialCellConfig[0].length; j++) {
-        initialCellConfig[i][j] = possibleStates[Integer.parseInt(initialStates[i * initialCellConfig[0].length + j])];
+        initialCellConfig[i][j] = possibleStates[Integer.parseInt(
+            initialStates[i * initialCellConfig[0].length + j])];
       }
     }
+  }
+
+  // Initialize an array that represents the desired neighborhood for a given reference cell
+  private void initializeNeighborConfig(String config) {
+    String[] neighbors = getArrayFromConfigString(config);
+
+    if (numNeighborRows * numNeighborColumns != neighbors.length) {
+      throw new IllegalArgumentException(
+          "Neighbor config size does not match number of rows and columns!");
+    }
+
+    neighborhoodConfig = new int[numNeighborRows][numNeighborColumns];
+
+    boolean foundReferenceCell = false;
+
+    for (int i = 0; i < numNeighborRows; i++) {
+      for (int j = 0; j < numNeighborColumns; j++) {
+        String currentNeighbor = neighbors[i * numNeighborColumns + j];
+
+        switch (currentNeighbor) {
+          case "0" -> neighborhoodConfig[i][j] = 0;
+          case "1" -> neighborhoodConfig[i][j] = 1;
+          case "X" -> {
+            // Only add central reference cell if it has not been seen before, else we have bad input
+            if (!foundReferenceCell) {
+              neighborhoodConfig[i][j] = 2;
+              foundReferenceCell = true;
+            } else {
+              throw new RuntimeException("Multiple reference cells found in neighborhood config.");
+            }
+          }
+        }
+      }
+    }
+  }
+
+  private String[] getArrayFromConfigString(String config) {
+    String trimmedConfig = config.trim().replaceAll("\\s+", " ");
+    return trimmedConfig.split(" ");
   }
 
   protected int getSimulationType() {
