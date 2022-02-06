aa3d32ac3598664139e7a9e774e0a5b0845ae871
Edison Ooi <edison.ooi@duke.edu>
2022-02-06 18:24:33 -0500

    Added parameter to determine wrapping

--- src/main/java/cellsociety/Model/SimulationData.java
+++ src/main/java/cellsociety/Model/SimulationData.java
@@ -3,20 +3,32 @@ package cellsociety.Model;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Simple immutable value object representing simulation instance data.
+ *
+ * @author Edison Ooi
+ */
 public record SimulationData(int simulationType, int numRows, int numColumns, String title,
                              String description,
                              String author, int numNeighborRows, int numNeighborColumns,
                              String neighborConfig, String startingConfig,
-                             Map<String, String> params) {
+                             Map<String, String> params, boolean shouldWrap) {
 
   // Name in data file that will indicate it represents data for this type of object
   public static final String DATA_TYPE = "CellAutomata";
+
   // Field names expected to appear in data file holding values for this object. These fields are commons across all SimulationTypes
   public static final List<String> GENERIC_DATA_FIELDS = List.of("SimulationType", "NumberOfRows",
       "NumberOfColumns",
       "Title", "Description", "Author", "NumberOfNeighborRows", "NumberOfNeighborColumns",
-      "NeighborConfig", "StartingConfig");
+      "NeighborConfig", "StartingConfig", "ShouldWrap");
 
+  /**
+   * Create simulation data from key value pairs in Maps extracted from XML.
+   *
+   * @param genericValues key-value pairs associated with parameters that all simulations have
+   * @param simulationParams key-value pairs associated with parameters for specific simulations
+   */
   public SimulationData(Map<String, String> genericValues, Map<String, String> simulationParams) {
     this(Integer.parseInt(genericValues.get(GENERIC_DATA_FIELDS.get(0))),
         Integer.parseInt(genericValues.get(GENERIC_DATA_FIELDS.get(1))),
@@ -28,7 +40,8 @@ public record SimulationData(int simulationType, int numRows, int numColumns, St
         Integer.parseInt(genericValues.get(GENERIC_DATA_FIELDS.get(7))),
         genericValues.get(GENERIC_DATA_FIELDS.get(8)),
         genericValues.get(GENERIC_DATA_FIELDS.get(9)),
-        simulationParams);
+        simulationParams,
+        Boolean.getBoolean(GENERIC_DATA_FIELDS.get(10)));
   }
 
 }
