84dfb850848537d9f07fa2f5c7816227ac5e33c1
Haris Adnan <ha109@duke.edu>
2022-02-06 01:32:47 -0800

    added imports to Selector

--- src/main/java/cellsociety/Selector.java
+++ src/main/java/cellsociety/Selector.java
@@ -1,4 +1,19 @@
 package cellsociety;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import cellsociety.GUIController.Language;
+import cellsociety.GUIController.Theme;
+import java.util.ResourceBundle;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Pane;
 
 public class Selector {
 
