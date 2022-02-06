c62d160c39e28afa1c607954946ac3689d15ea53
Haris Adnan <ha109@duke.edu>
2022-02-05 23:13:08 -0800

    Created Displayer Class

---
+++ src/main/java/cellsociety/Displayer.java
@@ -0,0 +1,22 @@
+package cellsociety;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
+import javafx.collections.ObservableList;
+import java.util.ResourceBundle;
+/**
+ * This class builds the GUI of the software
+ * @author Haris Adnan
+ * @author Matthew Knox
+ * @author Edison Ooi
+ */
+
+public class Displayer extends Scene {
+
+    
+
+}
